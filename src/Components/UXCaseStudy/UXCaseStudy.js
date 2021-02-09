99f36c74dc018deb7e021c547e6bd7f4863a4aa5
samSterns <sam.g.sterns@gmail.com>
2021-02-09 09:27:51 -0800

    still 2 previous bugs but added to case study

--- src/Components/UXCaseStudy/UXCaseStudy.js
+++ src/Components/UXCaseStudy/UXCaseStudy.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './UXCaseStudy.scss';
 import Pic from './logoHome.svg';
+import beforeImg1 from './assets/beforeImg.png';
 
 const UXCaseStudy = () => {
 
@@ -25,24 +26,40 @@ const darkMode = getPrefColorScheme();
             <p className="sub-title">Audit of usability issues and accessability requirements for a B2B start-up including designing the solutions, and implemented the code.</p>
 
             <div className="wrapper">
-              <h2>Process</h2>
+              <h2>Design and Development Process</h2>
               <ul>
-                <li>I completed the audit using WAVE and manual testing</li>
-                <li>I tested my solutions with tools like google's accessability dev tools, contrast checkers, WCAG2.0 AA standards, my own design project-specific heuristics, and usability testing.</li>
+              <h3>Research</h3>
+                <li>Conducted research about how to do an audit </li>
+                <li>Met with the clients and users to understand the problems</li>
+                <li> Completed the audit using WAVE and manual testing </li>
+              <h3>Analysis</h3>
+                <h4>Tested my solutions with:</h4>
+                  <ul>
+                    <li>Google's accessability dev tools</li>
+                    <li>Contrast checkers</li>
+                    <li>WCAG 2.0 AA standards</li>
+                    <li>Project-specific Design heuristics</li>
+                  </ul> 
+              <h3>Test</h3>
+                <li>Created mock-ups in Figma</li>
+                <li>Executed lean usability testing</li>
+              <h3>Deliver</h3>
+                <li>Implemented code in Scss, JSX, React.js</li>
               </ul>
             </div>
             <div className="wrapper">
               <h2>Color Contrast</h2>
               <ul>
-                <li></li>
-                <li></li>
+                <li><img style={{height: '250px'}} src={beforeImg1} alt="back to main page" /></li>
+                <li>Throughout the application I cleaned up the color choices by ensuring the correct contract ratios and limiting the number of accent colors</li>
+                <li>To increase usability I ensured button consistency too. Some buttons looks more like links because they lacked an outlines while the status updates looked like buttons</li>
               </ul>
             </div>
             <div className="wrapper">
               <h2>Information Architecture</h2>
               <ul>
-                <li></li>
-                <li></li>
+                <li>Correctly implemented semantic HTML with the use of header tags and landmarks</li>
+                <li>Taught the development team about not using headers for styling purposes</li>
               </ul>
             </div>
             <div className="wrapper">
